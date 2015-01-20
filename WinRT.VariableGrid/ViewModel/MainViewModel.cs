a1e12045ce0616d042b87afbda33fa317c643563
Tim Gabrhel <tim.gabrhel@outlook.com>
2015-01-19 21:28:19 -0600

    Revert "Updated to use larger than 1x1 item container for more efficiencies."
    This reverts commit 972392e91df7530c369ba78f6434b63dd258e87c.

--- WinRT.VariableGrid/ViewModel/MainViewModel.cs
+++ WinRT.VariableGrid/ViewModel/MainViewModel.cs
@@ -6,8 +6,6 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Input;
-using Windows.ApplicationModel;
 using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using WinRT.VariableGrid.Model;
@@ -24,14 +22,8 @@ namespace WinRT.VariableGrid.ViewModel
             set { SetProperty(ref _items, value); }
         }
 
-        public double BaseItemWidth { get { return Item.BaseWidth; } }
-
-        public double BaseItemHeight { get { return Item.BaseHeight; } }
-        
         public MainViewModel()
         {
-            if (DesignMode.DesignModeEnabled) return;
-
             Items = new ObservableCollection<Item>();
 
             var random = new Random();
@@ -46,7 +38,7 @@ namespace WinRT.VariableGrid.ViewModel
 
             RemeasureItems();
         }
-
+        
         /// <summary>
         /// Enumerates over each item, and re-runs the logic to determine the dynamic sizes of the items
         /// </summary>
@@ -67,14 +59,21 @@ namespace WinRT.VariableGrid.ViewModel
         /// <param name="i"></param>
         public void MeasureItem(Item item, int i)
         {
-            item.SpanSize = Item.MediumSize;
+            var width = (CoreApplication.MainView.CoreWindow.Bounds.Width / 2);
+            var height = width * 0.7941176470588235;
+
+            Size size = new Size(width, height);
+
+            item.Width = (int)size.Width;
+            item.Height = (int)size.Height;
 
             if ((i % 5) == 0)
             {
-                item.SpanSize = Item.LargeSize;
+                item.Width = (int)size.Width * 2;
+                //item.Height = (int)size.Height * 2;
             }
         }
-        
+
         #region INotifyPropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
 
